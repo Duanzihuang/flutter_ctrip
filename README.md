472c221ed2a8d6e8481d011f16b2d6515e46959d
manageritcast <manageritcast@126.com>
2019-11-27 11:19:25 +0800

    2-完成了网络请求及ListView的展示

--- README.md
+++ README.md
@@ -14,3 +14,76 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Dart
+> https://dartpad.dev/
+
+> 注意事项
+
+```
+1、Dart未初始化的默认类型是 null，JavaScript未初始化的默认类型是 undefined
+
+2、Dart中只有Boolean类型的true才是 true，JavaScript中非null 和 非0 都认为是true
+
+3、?.运算符在左边为null的情况下会阻断右边的调用(相当于js的 &&)，??运算符主要作用是在左边的表达式为null时为其设置默认值
+```
+
+> 异步编程【Future、Async、Await】
+
+```
+JavaScript中Promise是异步编程
+
+Dart中Future是异步编程
+
+JavaScript中 async 后面返回的是一个Promise对象，await 等待的是Promise执行完毕
+Dart中 async 后面返回的是一个Future对象，await 等待的是Future执行完毕
+```
+
+## Flutter
+
+> 基础知识
+
+```
+# 创建项目
+flutter create project_name
+
+# 运行项目(终端)
+flutter run -d '模拟器/真机名称'
+```
+
+> Widget
+
+```
+Flutter 中所有的视图都是由一个一个的小部件(Widget)组成的
+
+Widget 就是 iOS、Android、RN 中的View
+
+
+StatelessWidgets适用于当我们描述的用户界面不依赖于对象中的配置信息时。
+
+例如，在Android/iOS中，我们需要用ImageView/UIImageView来显示logo。 logo在运行时不会改变，因此在Flutter中使用StatelessWidget是最好不过了。
+
+如果要根据HTTP网络请求或用户交互后收到的数据动态更改UI，则必须使用StatefulWidget并告诉Flutter框架Widget的状态已更新，以便更新该Widget。
+
+无状态Widget和有状态Widget之间的重要区别在于StatefulWidgets具有一个State对象，该对象存储状态数据并将其传递到树重建中，因此状态不会丢失。
+```
+
+> 项目结构和依赖
+
+```
+pubspec.yaml 
+	Flutter项目的配置文件，里面包含了项目所需要的依赖，相当于RN中的 package.json 文件
+	
+依赖包下载地址
+	https://pub.dev/flutter
+	
+获取项目所需要的依赖
+	1、在 pubspec.yaml 中的 dependencies 配置好依赖
+			dependencies:
+				cupertino_icons: ^0.1.2
+  			http: ^0.12.0+2
+  			
+  2、在项目根目录执行
+  		flutter packages get
+```
+
---
+++ lib/main2.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+//void main() => runApp(MyApp());
+
+void main() => {
+      _getIPAddress(),
+
+      runApp(MyApp())
+  //  runApp(Center(
+    //  child: Text(
+    //    'Hello World',
+    //    textDirection: TextDirection.ltr,
+    //  ),
+  //))
+    };
+
+  _getIPAddress() async {
+    final url = 'https://httpbin.org/ip';
+
+    HttpClient httpClient = new HttpClient();
+
+    //打开Http连接
+    HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
+
+    //等待连接服务器，会将请求信息发送给服务器
+    HttpClientResponse response = await request.close();
+    //读取响应内容
+    var _text = await response.transform(utf8.decoder).join();
+
+    print('ip地址如下:');
+    print(_text);
+
+    //关闭client后，通过该client发起的所有请求都会中止
+    httpClient.close();
+  }
+
+  class MyApp extends StatelessWidget {
+    // This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      );
+    }
+  }
+
+  class MyHomePage extends StatefulWidget {
+    MyHomePage({Key key, this.title}) : super(key: key);
+
+    // This widget is the home page of your application. It is stateful, meaning
+    // that it has a State object (defined below) that contains fields that affect
+    // how it looks.
+
+    // This class is the configuration for the state. It holds the values (in this
+    // case the title) provided by the parent (in this case the App widget) and
+    // used by the build method of the State. Fields in a Widget subclass are
+    // always marked "final".
+
+    final String title;
+
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+  }
+
+  class _MyHomePageState extends State<MyHomePage> {
+    int _counter = 0;
+
+    void _incrementCounter() {
+      setState(() {
+        // This call to setState tells the Flutter framework that something has
+        // changed in this State, which causes it to rerun the build method below
+        // so that the display can reflect the updated values. If we changed
+        // _counter without calling setState(), then the build method would not be
+        // called again, and so nothing would appear to happen.
+        _counter++;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      // This method is rerun every time setState is called, for instance as done
+      // by the _incrementCounter method above.
+      //
+      // The Flutter framework has been optimized to make rerunning build methods
+      // fast, so that you can just rebuild anything that needs updating rather
+      // than having to individually change instances of widgets.
+      return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
+  }
